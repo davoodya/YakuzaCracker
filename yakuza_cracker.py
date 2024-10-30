246b55166b8f695fd60af85f128043dcc515b71d
davoodya <davoodya40@gmail.com>
2024-10-30 22:47:54 +0330

    Implement Step 4: Function to Get the Path to Bundled Resources in PyInstaller

--- yakuza_cracker.py
+++ yakuza_cracker.py
@@ -8,9 +8,10 @@ Last Update: 29/10/2024 --- 8 aban 1403"""
 
 """ Step 0: Import all Requires Libraries """
 import sys
+import logging
 
 from colorama import init
-import logging
+from os import path
 
 """ Step 1: Initialize colorama for colored console output """
 init()
@@ -32,7 +33,22 @@ results = []
 """ Section 2: Developing Utility Functions """
 
 """ Step 4: Function to Get the Path to Bundled Resources in PyInstaller """
+def resource_path(relative_path):
+    try:
+        # if gui.py bundled with PyInstaller,
+        # to get the current path of where gui.py run in there, we should use sys._MEIPASS
+        if hasattr(sys, "_MEIPASS"):
+            basePath = sys._MEIPASS     # noqa
 
+        # if a python file runs in the IDE or Console, then to get the current path of where gui.py run in there,
+        # We should use __file__ in the dirname(abspath(__file__))
+        else:
+            basePath = path.dirname(path.abspath(__file__))
+
+    except Exception:   # noqa
+        basePath = path.abspath(".")
+
+    return path.join(basePath, relative_path)
 
 
 
