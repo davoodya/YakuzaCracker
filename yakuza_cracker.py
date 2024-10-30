28dabfa6a78e5d97a466b854536da5da8ae3abbd
davoodya <davoodya40@gmail.com>
2024-10-30 23:00:57 +0330

    Implement Step 5: Function to Try a Password on Different File Types

--- yakuza_cracker.py
+++ yakuza_cracker.py
@@ -6,33 +6,29 @@ Last Update: 29/10/2024 --- 8 aban 1403"""
 
 """ Section 1: Import all Requires Libraries and Basic Configurations """
 
-""" Step 0: Import all Requires Libraries """
+# Step 0: Import all Requires Libraries 
 import sys
 import logging
 
 from colorama import init
 from os import path
 
-""" Step 1: Initialize colorama for colored console output """
+# Step 1: Initialize colorama for colored console output 
 init()
 
-""" Step 2: Setup logging configuration """
+# Step 2: Setup logging configuration
 logging.basicConfig(filename="yakuza_cracker.log", level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
-""" Step 3: Define global variables """
+# Step 3: Define global variables
+stopFlag = False    # stopFlag used to stop the cracking process
 
-# stopFlag used to stop the cracking process
-stopFlag = False
-
-# result's list use to store the cracked(founded) passwords
-results = []
-
+results = []    # result's list use to store the cracked(founded) passwords
 
 
 """ Section 2: Developing Utility Functions """
 
-""" Step 4: Function to Get the Path to Bundled Resources in PyInstaller """
+# Step 4: Function to Get the Path to Bundled Resources in PyInstaller
 def resource_path(relative_path):
     try:
         # if gui.py bundled with PyInstaller,
@@ -49,6 +45,44 @@ def resource_path(relative_path):
         basePath = path.abspath(".")
 
     return path.join(basePath, relative_path)
+
+
+""" Step 5: Function to Try a Password on Different File Types """
+def try_password(file_path, file_type, password):
+    logging.info(f"[+] Trying password: {password}")
+    
+    try:
+        # If the file is an Office file, then execute try_office_password
+        if file_type in ['xls', 'xlsx', 'doc', 'docx']:
+            return try_office_password(file_path, password)
+
+        # If the file is ZIP File, then execute try_zip_password()
+        elif file_type == 'zip':
+            return try_zip_password(file_path, password)
+
+        # If the file is PDF File, then execute try_pdf_password()
+        elif file_type == 'pdf':
+            return try_pdf_password(file_path, password)
+
+        else:
+            logging.error("[-] Unsupported file type.")
+            return False
+
+    except Exception as e:
+        logging.error(f"[-] Error Trying password: {password} | Error: {e}")
+        return False
+
+
+def try_office_password(file_path, password):
+    pass
+
+
+def try_zip_password(file_path, password):
+    pass
+
+
+def try_pdf_password(file_path, password):
+    pass
 
 
 
