b9577071924a55aa44e5fd94bee5bbd0d4ea7ca5
davoodya <davoodya40@gmail.com>
2024-10-30 21:24:24 +0330

    Start Developing the main Application; we used GUI templated developed in the Section 0(gui.py)

---
+++ yakuza_cracker.py
@@ -0,0 +1,11 @@
+""" YakuzaCracker - an Advanced GUI Tool for Cracking ZIP, RAR and MS-Office Documents,
+    Also yakuzaCracker can Perform Reverse Bruteforce Attacks to Crack the Login Pages on the http and https.
+
+Author: Davood Yakuza from Iran, Isfahan
+Last Update: 29/10/2024 --- 8 aban 1403"""
+
+""" Step 0: Import all Requires Libraries """
+import sys
+
+from colorama import init
+
